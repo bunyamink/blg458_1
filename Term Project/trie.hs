77cd6ee0bb66b60a59cdefe0b7a7fec7cb0ef267
bunyamink <bunyaminibrahim@gmail.com>
2018-05-22 00:28:07 +0300

    menu included searh and add parts done.

--- Term Project/trie.hs
+++ Term Project/trie.hs
@@ -28,21 +28,20 @@ search []     trie@(Trie e _) = e
 search (x:xs) trie@(Trie _ m) = fromMaybe False (search xs <$> M.lookup x m)
 
 getWords :: Trie -> [Word]
-getWords trie@(Trie e m)= map (fst children) . filter e . M.toList m
+getWords trie@(Trie e m)= getWords' (M.toList m) []
+  where
+    --getWords' :: [] -> [] -> [Words]
+    getWords' (x:xs) w = undefined
+
+d = insertList ["ali", "abla", "baba", "sade"]
+ch = children d
+ts = M.toList ch
 
 prefix :: Word -> Trie -> Maybe [Word]
 prefix [x]     trie@(Trie e _)     = Just ["s"] 
 prefix (x:xs)  trie@(Trie e _)     = case M.lookup x (children trie) of
                                           Nothing -> Nothing
                                           Just t' -> prefix xs t'
-
-  
-ss = do selected <- getLine
-        case selected of "e" -> return "e"
-                         "a" -> return "a"
-                         "f" -> return "f"
-                         "s" -> return "s"
-                         "p" -> return "p"
         
 
 printMenu = do putStrLn "a) Add word"
@@ -50,8 +49,27 @@ printMenu = do putStrLn "a) Add word"
                putStrLn "f) Find words with prefix"
                putStrLn "p) print all words"
                putStrLn "e) exit"
+               putStrLn "Enter Action: "
 
 
 main = do args <- getArgs
-          let incomming = head args
-          putStrLn incomming
+          let fileName = head args
+          handle <- openFile fileName ReadMode
+          contents <- hGetContents handle
+          let singlewords = words contents
+          --print singlewords
+          let mainTrie = insertList singlewords
+          --let mainTrie = insertList $ words contents
+          --print mainTrie
+          printMenu
+          selected <- getLine
+          if selected == "a" then
+            do putStrLn "Enter Word/Prefix:"
+               wrd <- getLine
+               let x = insert wrd mainTrie
+               print x
+            else if selected == "s" then
+              do putStrLn "Enter Word/Prefix:"
+                 wrd <- getLine
+                 if search wrd mainTrie then putStrLn "Exists in vocabulary" else putStrLn "NOT Exists!"
+                   else do putStrLn "sss"
